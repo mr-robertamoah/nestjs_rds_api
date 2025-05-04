4f289b53ecb9bd971d3b8e7d3612677460e6662f
Robert Amoah <mr_robertamoah@yahoo.com>
2025-05-04 23:35:24 +0000

    feat: Add nestjs/config to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MysqlService } from './mysql/mysql.service';
 import { ReportsController } from './reports/reports.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, ReportsController],
   providers: [AppService, MysqlService],
 })
